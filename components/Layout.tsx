bef1ad3a4adadb57977b429a8c3155bddbf057fa
jonnyabrams <jonnyabrams@hotmail.com>
2022-08-28 10:03:25 +0100

    feat: Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,28 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import Head from "next/head";
+import { ReactElement } from "react";
+
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+type LayoutProps = {
+  children: ReactElement<any, any>;
+};
+
+const Layout = ({children, ...LayoutProps}: LayoutProps) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Get Your Gadgets</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
