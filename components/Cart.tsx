360ae4f18eb39c932ecb57c0e221f7c11f13fb6c
jonnyabrams <jonnyabrams@hotmail.com>
2022-09-09 10:39:48 -0400

    feat: fix price formatting

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -89,7 +89,7 @@ const Cart = () => {
         {cartItems.length >= 1 && <div className="cart=bottom">
           <div className="total">
             <h3>Subtotal:</h3>
-            <h3>${totalPrice}</h3>
+            <h3>${totalPrice.toFixed(2)}</h3>
           </div>
           <div className="btn-container">
             <button type="button" className="btn" onClick={() => {}}>
